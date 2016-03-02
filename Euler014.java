05216348c2b11ad41a62ea99f5e17a63cd8a471b
meschke <tim.mesche@hotmail.de>
2016-03-02 12:21:48 +0100

    task 014
    Build a Map to check for recurring sequences and their number of elements.
    
    - Map<Integer, Integer> = new Hashmap
    - cool algorithm

--- Euler014.java
+++ Euler014.java
@@ -1,37 +1,44 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Euler014 {
 	public static void main (String[] args) {
-		ArrayList<Integer> sequence = new ArrayList<Integer>();
-		ArrayList<Integer> longestSequence = new ArrayList<Integer>();
 		int number = 0;
-		int countLongestChain = 0;
+		int countChain = 0;
+		int longestSequenceKey = 1;
+		boolean isSequenceEnd = false;
+		Map<Integer, Integer> mapNumberValues = new HashMap<Integer, Integer>();		// k = number, v = number of its sequence elements
+		
+		mapNumberValues.put(1, 1);
 
-		for (int i = 1; i <= 106239; i++) {
-		    sequence.clear();
+		for (int i = 2; i <= 1000000; i++) {
+		    isSequenceEnd = false;
+		    countChain = 0;
 			number = i;
-			sequence.add(number);
-			while (number != 1) {
-				
-				if (number % 2 == 0) {
-					number /= 2;
-					sequence.add(number);
+			
+			while (!isSequenceEnd) {
+				if (mapNumberValues.containsKey(number)) {
+					countChain += mapNumberValues.get(number);
+					isSequenceEnd = true;
+					break;
 				} else {
-					if (number % 2 != 0) {
-						number = 3 * number + 1;
-						sequence.add(number);
-					}
+					if (number % 2 == 0) {
+						number /= 2;
+						countChain++;
+					} else {
+						if (number % 2 != 0) {
+							number = number * 3 + 1;
+							countChain++;
+						} 
+					}	
 				}
 			}
-
-			if (sequence.size() > countLongestChain) {
-				    countLongestChain = sequence.size();
-				    longestSequence.clear();
-				    longestSequence.addAll(sequence);
-				    
-				    System.out.println("Longest sequence until starting number " +i+ " => " +longestSequence);
-		            System.out.println("It contains " +countLongestChain+ " numbers.");
+			
+			if (countChain > mapNumberValues.get(longestSequenceKey)) {
+				longestSequenceKey = i;
 			}
+			mapNumberValues.put(i, countChain);
 		}
+		
+		System.out.println("The number " +longestSequenceKey+ " has the longest sequence with " + mapNumberValues.get(longestSequenceKey) + " elements.");
 	}	
 }
